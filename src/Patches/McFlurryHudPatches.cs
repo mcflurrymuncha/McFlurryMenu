8eea6558efa96a85210bfde192862c227a67b675
mcflurry <vexithereal@gmail.com>
2026-04-11 10:57:06 +0100

    Update and rename HudManagerPatches.cs to McFlurryHudPatches.cs

---
+++ src/Patches/McFlurryHudPatches.cs
@@ -0,0 +1,53 @@
+using HarmonyLib;
+using System;
+
+namespace McFlurryMenu;
+
+[HarmonyPatch(typeof(HudManager), nameof(HudManager.Start))]
+public static class HudManager_Start
+{
+    // Postfix patch of HudManager.Start to give minimap access to impostors too
+    public static void Postfix(HudManager __instance)
+    {
+        __instance.MapButton.OnClick.RemoveAllListeners(); // Remove previous OnClick action
+
+        // Always open normal map when map button is clicked
+        // To access sabotage map, sabotage button can be used
+        __instance.MapButton.OnClick.AddListener((Action) (() =>
+        {
+            __instance.ToggleMapVisible(new MapOptions
+            {
+                Mode = MapOptions.Modes.Normal
+            });
+        }));
+    }
+}
+
+[HarmonyPatch(typeof(HudManager), nameof(HudManager.Update))]
+public static class HudManager_Update
+{
+    public static void Postfix(HudManager __instance)
+    {
+        __instance.ShadowQuad.gameObject.SetActive(!McFlurryESP.IsFullbrightActive()); // Fullbright
+
+        if (Utils.IsChatUiActive()) // AlwaysChat
+        {
+            __instance.Chat.gameObject.SetActive(true);
+        }
+        else
+        {
+            Utils.CloseChat();
+            __instance.Chat.gameObject.SetActive(false);
+        }
+
+        McFlurryCheats.UseVentCheat(__instance);
+        McFlurryESP.ZoomOut(__instance);
+        McFlurryESP.FreecamCheat();
+
+        // Close PlayerPickMenu if there is no PPM cheat enabled
+        if (PlayerPickMenu.playerpickMenu != null && CheatToggles.ShouldPPMClose())
+        {
+            PlayerPickMenu.playerpickMenu.Close();
+        }
+    }
+}
