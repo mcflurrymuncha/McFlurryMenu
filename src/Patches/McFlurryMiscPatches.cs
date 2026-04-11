09d05ae58c54e65913fe75d31e5d30a2a80a8372
mcflurry <vexithereal@gmail.com>
2026-04-11 11:02:00 +0100

    Update and rename OtherPatches.cs to McFlurryMiscPatches.cs

--- src/Patches/McFlurryMiscPatches.cs
+++ src/Patches/McFlurryMiscPatches.cs
@@ -7,7 +7,7 @@ using System;
 using System.Security.Cryptography;
 using InnerNet;
 
-namespace MalumMenu;
+namespace McFlurryMenu;
 
 [HarmonyPatch(typeof(Constants), nameof(Constants.GetPlatformData))]
 public static class Constants_GetPlatformData
@@ -15,7 +15,7 @@ public static class Constants_GetPlatformData
     // Postfix patch of Constants.GetPlatformData to spoof the user's platform type
     public static void Postfix(ref PlatformSpecificData __result)
     {
-        if (Utils.StringToPlatformType(MalumMenu.spoofPlatform.Value, out Platforms? platformType))
+        if (Utils.StringToPlatformType(McFlurryMenu.spoofPlatform.Value, out Platforms? platformType))
         {
             __result = new PlatformSpecificData
             {
@@ -32,10 +32,8 @@ public static class FreeChatInputField_UpdateCharCount
     // Postfix patch of FreeChatInputField.UpdateCharCount to change how charCountText displays
     public static void Postfix(FreeChatInputField __instance)
     {
-        // Only works if CheatToggles.longerMsgs is enabled
         if (!CheatToggles.longerMessages) return;
 
-        // Update charCountText to account for longer characterLimit
         int length = __instance.textArea.text.Length;
         __instance.charCountText.SetText($"{length}/{__instance.textArea.characterLimit}");
 
@@ -58,19 +56,18 @@ public static class FreeChatInputField_UpdateCharCount
 public static class ChatBubble_SetName
 {
     public static void Postfix(ChatBubble __instance)
-	{
-        MalumESP.ChatNametags(__instance);
+    {
+        McFlurryESP.ChatNametags(__instance);
     }
 }
 
 [HarmonyPatch(typeof(SystemInfo), nameof(SystemInfo.deviceUniqueIdentifier), MethodType.Getter)]
 public static class SystemInfo_deviceUniqueIdentifier_Getter
 {
-    // Postfix patch of SystemInfo.deviceUniqueIdentifier Getter method
-    // Made to hide the user's real unique deviceId by generating a random fake one
+    // Postfix patch to hide the user's real unique deviceId
     public static void Postfix(ref string __result)
     {
-        if (!MalumMenu.spoofDeviceId.Value) return;
+        if (!McFlurryMenu.spoofDeviceId.Value) return;
 
         var bytes = new byte[16];
         using (var rng = RandomNumberGenerator.Create())
@@ -85,18 +82,21 @@ public static class SystemInfo_deviceUniqueIdentifier_Getter
 [HarmonyPatch(typeof(VersionShower), nameof(VersionShower.Start))]
 public static class VersionShower_Start
 {
-    // Postfix patch of VersionShower.Start to show MalumMenu version
+    // Postfix patch to show McFlurryMenu version
     public static void Postfix(VersionShower __instance)
     {
-        if (MalumMenu.inStealthMode || MalumMenu.isPanicked) return;
+        if (McFlurryMenu.inStealthMode || McFlurryMenu.isPanicked) return;
 
-        if (MalumMenu.supportedAU.Contains(Application.version)) // Checks if Among Us version is supported
+        string brand = "McFlurryMenu";
+        string version = McFlurryMenu.mcFlurryVersion;
+
+        if (McFlurryMenu.supportedAU.Contains(Application.version))
         {
-            __instance.text.text =  $"MalumMenu v{MalumMenu.malumVersion} (v{Application.version})"; // Supported
+            __instance.text.text = $"{brand} v{version} (v{Application.version})";
         }
         else
         {
-            __instance.text.text =  $"MalumMenu v{MalumMenu.malumVersion} (<color=red>v{Application.version}</color>)"; // Unsupported
+            __instance.text.text = $"{brand} v{version} (<color=red>v{Application.version}</color>)";
         }
     }
 }
@@ -104,42 +104,37 @@ public static class VersionShower_Start
 [HarmonyPatch(typeof(PingTracker), nameof(PingTracker.Update))]
 public static class PingTracker_Update
 {
-    // Postfix patch of PingTracker.Update to show MalumMenu authors and colored ping text
+    // Postfix patch to show McFlurryMenu authors and colored ping text
     public static void Postfix(PingTracker __instance)
     {
-        if (MalumMenu.inStealthMode)
+        if (McFlurryMenu.inStealthMode)
         {
             __instance.text.alignment = TMPro.TextAlignmentOptions.TopLeft;
-
             return;
         }
 
         __instance.text.alignment = TMPro.TextAlignmentOptions.Center;
+        string credits = "McFlurryMenu by scp222thj & Astral";
 
         if (AmongUsClient.Instance.IsGameStarted)
         {
             __instance.aspectPosition.DistanceFromEdge = new Vector3(-0.21f, 0.50f, 0f);
-
-            __instance.text.text = $"MalumMenu by scp222thj & Astral ~ {Utils.GetColoredPingText(AmongUsClient.Instance.Ping)}";
-
+            __instance.text.text = $"{credits} ~ {Utils.GetColoredPingText(AmongUsClient.Instance.Ping)}";
             return;
         }
 
-        __instance.text.text = $"MalumMenu by scp222thj & Astral\n{Utils.GetColoredPingText(AmongUsClient.Instance.Ping)}";
-
+        __instance.text.text = $"{credits}\n{Utils.GetColoredPingText(AmongUsClient.Instance.Ping)}";
     }
 }
 
 [HarmonyPatch(typeof(DisconnectPopup), nameof(DisconnectPopup.DoShow))]
 public static class DisconnectPopup_DoShow
 {
-    // Postfix patch of DisconnectPopup.DoShow to copy lobby code to clipboard on disconnect
     public static void Postfix(DisconnectPopup __instance)
     {
         if (!CheatToggles.copyLobbyCodeOnDisconnect) return;
 
         GUIUtility.systemCopyBuffer = AmongUsClient_OnGameJoined.lastGameIdString;
-
         __instance.SetText(__instance._textArea.text + "\n\n<size=60%>Lobby code has been copied to the clipboard</size>");
     }
 }
@@ -147,46 +142,36 @@ public static class DisconnectPopup_DoShow
 [HarmonyPatch(typeof(PlayerBanData), nameof(PlayerBanData.BanMinutesLeft), MethodType.Getter)]
 public static class PlayerBanData_BanMinutesLeft_Getter
 {
-    // Postfix patch of PlayerBanData.BanMinutesLeft Getter method to remove disconnect penalty
     public static void Postfix(PlayerBanData __instance, ref int __result)
     {
         if (!CheatToggles.avoidPenalties) return;
 
-        __instance.BanPoints = 0f; // Removes all BanPoints
-        __result = 0; // Removes all BanMinutes
+        __instance.BanPoints = 0f;
+        __result = 0;
     }
 }
 
 [HarmonyPatch(typeof(FullAccount), nameof(FullAccount.CanSetCustomName))]
 public static class FullAccount_CanSetCustomName
 {
-    // Prefix patch of FullAccount.CanSetCustomName to allow the usage of custom names
     public static void Prefix(ref bool canSetName)
     {
-        if (CheatToggles.unlockFeatures)
-        {
-            canSetName = true;
-        }
+        if (CheatToggles.unlockFeatures) canSetName = true;
     }
 }
 
 [HarmonyPatch(typeof(AccountManager), nameof(AccountManager.CanPlayOnline))]
 public static class AccountManager_CanPlayOnline
 {
-    // Prefix patch of AccountManager.CanPlayOnline to allow online games
     public static void Postfix(ref bool __result)
     {
-        if (CheatToggles.unlockFeatures)
-        {
-            __result = true;
-        }
+        if (CheatToggles.unlockFeatures) __result = true;
     }
 }
 
 [HarmonyPatch(typeof(InnerNetClient), nameof(InnerNetClient.JoinGame))]
 public static class InnerNetClient_JoinGame
 {
-    // Prefix patch of InnerNetClient.JoinGame to allow online games
     public static void Prefix()
     {
         if (CheatToggles.unlockFeatures)
@@ -199,13 +184,10 @@ public static class InnerNetClient_JoinGame
 [HarmonyPatch(typeof(GameManager), nameof(GameManager.CheckTaskCompletion))]
 public static class GameManager_CheckTaskCompletion
 {
-    // Prefix patch of GameManager.CheckTaskCompletion to prevent a running game from ending
     public static bool Prefix(ref bool __result)
     {
         if (!CheatToggles.noGameEnd) return true;
-
         __result = false;
-
         return false;
     }
 }
@@ -215,89 +197,59 @@ public static class Mushroom_FixedUpdate
 {
     public static void Postfix(Mushroom __instance)
     {
-        MalumESP.SporeCloudVision(__instance);
+        McFlurryESP.SporeCloudVision(__instance);
     }
 }
 
-// Found here: https://github.com/g0aty/SickoMenu/blob/main/hooks/PlainDoor.cpp
 [HarmonyPatch(typeof(DoorBreakerGame), nameof(DoorBreakerGame.Start))]
 public static class DoorBreakerGame_Start
 {
-    // Prefix patch of DoorBreakerGame.Start to automatically open a door when the player interacts with it
     public static bool Prefix(DoorBreakerGame __instance)
     {
         if (!CheatToggles.autoOpenDoorsOnUse) return true;
 
-        DoorsHandler.OpenDoor(__instance.MyDoor);
+        McFlurryDoorsHandler.OpenDoor(__instance.MyDoor);
         __instance.MyDoor.SetDoorway(true);
         __instance.Close();
-
         return false;
     }
 }
 
-// Found here: https://github.com/g0aty/SickoMenu/blob/main/hooks/PlainDoor.cpp
 [HarmonyPatch(typeof(DoorCardSwipeGame), nameof(DoorCardSwipeGame.Begin))]
 public static class DoorCardSwipeGame_Begin
 {
-    // Prefix patch of DoorCardSwipeGame.Begin to automatically open a door when the player interacts with it
     public static bool Prefix(DoorCardSwipeGame __instance)
     {
         if (!CheatToggles.autoOpenDoorsOnUse) return true;
 
-        DoorsHandler.OpenDoor(__instance.MyDoor);
+        McFlurryDoorsHandler.OpenDoor(__instance.MyDoor);
         __instance.MyDoor.SetDoorway(true);
         __instance.Close();
-
         return false;
     }
 }
 
-// Found here: https://github.com/g0aty/SickoMenu/blob/main/hooks/PlainDoor.cpp
 [HarmonyPatch(typeof(MushroomDoorSabotageMinigame), nameof(MushroomDoorSabotageMinigame.Begin))]
 public static class MushroomDoorSabotageMinigame_Begin
 {
-    // Prefix patch of MushroomDoorSabotageMinigame.Begin to automatically open a door when the player interacts with it
     public static bool Prefix(MushroomDoorSabotageMinigame __instance)
     {
         if (!CheatToggles.autoOpenDoorsOnUse) return true;
-
         __instance.FixDoorAndCloseMinigame();
-
         return false;
     }
 }
-
-// NEEDS FIX : Blocks usage of consoles to which impostor
-// has access to (like those to fix sabotages) when cheat is disabled
-
-// [HarmonyPatch(typeof(Console), nameof(Console.CanUse))]
-// public static class Console_CanUse
-// {
-//     // Prefix patch of Console.CanUse to allow impostors to do tasks
-//     public static void Prefix(Console __instance)
-//     {
-//         __instance.AllowImpostor = CheatToggles.impostorTasks;
-//     }
-// }
 
 [HarmonyPatch(typeof(IntroCutscene), "CoBegin")]
 public static class IntroCutscene_CoBegin
 {
-    // Prefix patch of IntroCutscene.CoBegin to force the LocalPlayer's role to a specified role
     public static void Prefix()
     {
         if (!Utils.isHost || !CheatToggles.forcedRole.HasValue) return;
 
         var forcedRole = CheatToggles.forcedRole.Value;
+        if (PlayerControl.LocalPlayer.Data.RoleType == forcedRole) return;
 
-        // If LocalPlayer already has the forced role, do nothing
-        if (PlayerControl.LocalPlayer.Data.RoleType == forcedRole)
-        {
-            return;
-        }
-
-        // Find a player with the forced role to swap roles with
         PlayerControl roleSwapTarget = null;
         foreach (var player in PlayerControl.AllPlayerControls)
         {
@@ -307,7 +259,6 @@ public static class IntroCutscene_CoBegin
         }
 
         DestroyableSingleton<RoleManager>.Instance.SetRole(PlayerControl.LocalPlayer, forcedRole);
-
         if (roleSwapTarget != null)
         {
             DestroyableSingleton<RoleManager>.Instance.SetRole(roleSwapTarget, PlayerControl.LocalPlayer.Data.RoleType);
@@ -318,40 +269,28 @@ public static class IntroCutscene_CoBegin
 [HarmonyPatch(typeof(AmongUsDateTime), nameof(AmongUsDateTime.UtcNow), MethodType.Getter)]
 public static class AmongUsDateTime_UtcNow
 {
-    // Prefix patch of AmongUsDateTime.UtcNow to spoof the date to April 2nd, 7:01 AM UTC
-    // This sets the map to dlekS ehT (The Skeld but flipped) when hosting a lobby
     public static bool Prefix(ref Il2CppSystem.DateTime __result)
     {
         if (!CheatToggles.spoofAprilFoolsDate) return true;
-
         var managedDate = new DateTime(DateTime.UtcNow.Year, 4, 2, 7, 1, 0, DateTimeKind.Utc);
         __result = new Il2CppSystem.DateTime(managedDate.Ticks);
-
         return false;
     }
 }
 
-// Found here: https://github.com/g0aty/SickoMenu/blob/main/hooks/LobbyBehaviour.cpp
 [HarmonyPatch(typeof(GameContainer), nameof(GameContainer.SetupGameInfo))]
 public static class GameContainer_SetupGameInfo
 {
-    // Postfix patch of GameContainer.SetupGameInfo to show more information when finding a game:
-    // host name (e.g. Astral), lobby code (e.g. KLHCEG), host platform (e.g. Epic), and lobby age in minutes (e.g. 4:20)
     public static void Postfix(GameContainer __instance)
     {
         if (!CheatToggles.seeLobbyInfo) return;
 
-        // The Crewmate icon gets aligned properly with this
         const string separator = "<#0000>000000000000000</color>";
-
         var trueHostName = __instance.gameListing.TrueHostName;
-
         var age = __instance.gameListing.Age;
         var lobbyTime = $"Age: {age / 60}:{(age % 60 < 10 ? "0" : "")}{age % 60}";
-
         var platform = Utils.PlatformTypeToString(__instance.gameListing.Platform);
 
-        // Sets the text of the capacity field to include the new information
         __instance.capacity.text = $"<size=40%>{separator}\n{trueHostName}\n{__instance.capacity.text}\n" +
                                    $"<#fb0>{GameCode.IntToGameName(__instance.gameListing.GameId)}</color>\n" +
                                    $"<#b0f>{platform}</color>\n{lobbyTime}\n{separator}</size>";
@@ -361,17 +300,13 @@ public static class GameContainer_SetupGameInfo
 [HarmonyPatch(typeof(BanMenu), nameof(BanMenu.SetVisible))]
 public static class BanMenu_SetVisible
 {
-    // Prefix patch of BanMenu.SetVisible to always show kick and ban buttons as host
     public static bool Prefix(BanMenu __instance, bool show)
     {
         if (!Utils.isHost) return true;
-
         show &= PlayerControl.LocalPlayer && PlayerControl.LocalPlayer.Data != null;
-
         __instance.BanButton.gameObject.SetActive(true);
         __instance.KickButton.gameObject.SetActive(true);
         __instance.MenuButton.gameObject.SetActive(show);
-
         return false;
     }
 }
@@ -379,13 +314,10 @@ public static class BanMenu_SetVisible
 [HarmonyPatch(typeof(IGameOptionsExtensions), nameof(IGameOptionsExtensions.GetAdjustedNumImpostors))]
 public static class IGameOptionsExtensions_GetAdjustedNumImpostors
 {
-    // Prefix patch of IGameOptionsExtensions.GetAdjustedNumImpostors to remove impostor limits
     public static bool Prefix(IGameOptions __instance, ref int __result)
     {
         if (!CheatToggles.noOptionsLimits) return true;
-
         __result = GameOptionsManager.Instance.CurrentGameOptions.NumImpostors;
-
         return false;
     }
 }
@@ -393,11 +325,9 @@ public static class IGameOptionsExtensions_GetAdjustedNumImpostors
 [HarmonyPatch(typeof(PlayerPurchasesData), nameof(PlayerPurchasesData.GetPurchase))]
 public static class PlayerPurchasesData_GetPurchase
 {
-    // Postfix patch of PlayerPurchasesData.GetPurchase to unlock all cosmetics
     public static void Postfix(ref bool __result)
     {
         if (!CheatToggles.freeCosmetics) return;
-
         __result = true;
     }
 }
