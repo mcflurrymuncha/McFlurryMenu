67126197bd9776a20f69b3b70ea0d13a0b2688d5
mcflurry <vexithereal@gmail.com>
2026-04-11 11:08:59 +0100

    Update ITab.cs

--- src/UI/Utilities/Structures/ITab.cs
+++ src/UI/Utilities/Structures/ITab.cs
@@ -1,7 +1,10 @@
-namespace MalumMenu;
+namespace McFlurryMenu;
 
 public interface ITab
 {
+    // The display name of the tab in the McFlurry selection bar
     string name { get; }
+
+    // The method called every frame to render the tab's specific buttons and toggles
     void Draw();
 }
