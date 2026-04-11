a51bd7338f27a1a0e32571f67bca385d1fc51ccd
mcflurry <vexithereal@gmail.com>
2026-04-11 10:57:52 +0100

    Update and rename LogicGameFlowPatches.cs to McFlurryGameFlowPatches.cs

--- src/Patches/McFlurryGameFlowPatches.cs
+++ src/Patches/McFlurryGameFlowPatches.cs
@@ -1,6 +1,6 @@
 using HarmonyLib;
 
-namespace MalumMenu;
+namespace McFlurryMenu;
 
 [HarmonyPatch(typeof(LogicGameFlowNormal), nameof(LogicGameFlowNormal.CheckEndCriteria))]
 public static class LogicGameFlowNormal_CheckEndCriteria
@@ -23,7 +23,6 @@ public static class LogicGameFlowNormal_IsGameOverDueToDeath
         {
             __result = false;
         }
-
     }
 }
 
@@ -48,6 +47,5 @@ public static class LogicGameFlowHnS_IsGameOverDueToDeath
         {
             __result = false;
         }
-
     }
 }
